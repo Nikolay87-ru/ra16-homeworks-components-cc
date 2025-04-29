201d859e803bf90811457efb7c7c711c14c70dd8
Neto test <nikolajeliseev834@gmail.com>
2025-04-29 19:27:30 +0300

    исправления в файлах

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,21 +1,22 @@
-import './App.css';
-import ShopItemClass from './components/ShopItemClass';
+import "./App.css";
+import ShopItemClass from "./components/ShopItemClass";
 
 function App() {
   const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    brand: "Tiger of Sweden",
+    title: "Leonard coat",
+    description: "Minimalistic coat in cotton-blend",
+    descriptionFull:
+      "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
     price: 399,
-    currency: '£'
+    currency: "£",
   };
 
   return (
     <div className="container">
       <div className="background-element"></div>
       <div className="highlight-window">
-        <div className='highlight-overlay'></div>
+        <div className="highlight-overlay"></div>
       </div>
       <div className="window">
         <ShopItemFunc item={item} />
--- my-app/src/components/types.ts
+++ my-app/src/components/types.ts
@@ -1,8 +1,12 @@
-export interface ShopItem {
+export interface IShopItem {
   brand: string;
   title: string;
   description: string;
   descriptionFull: string;
   price: number;
   currency: string;
+}
+
+export interface ShopItemProps {
+  item: IShopItem;
 }
